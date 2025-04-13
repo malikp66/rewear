06e21034b3c4c3ffe34104e5a4e4a1fabfba6bf1
malik <malikputra.mr@gmail.com>
2025-04-13 22:29:29 +0700

    fix docker

--- be/settings.py
+++ be/settings.py
@@ -82,6 +82,15 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+        'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
 }
 
 AUTH_USER_MODEL = "authentification.CustomUser"
