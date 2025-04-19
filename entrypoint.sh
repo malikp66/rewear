817cfbc5ee0d320b6b1f84a5e7b488088859f861
malik <malikputra.mr@gmail.com>
2025-04-20 01:11:12 +0700

    add more workers n thread

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,14 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-gunicorn be.wsgi:application --bind 127.0.0.1:8001 &
+gunicorn be.wsgi:application \
+  --bind 127.0.0.1:8001 \
+  --workers 8 \
+  --threads 4 \
+  --timeout 60 \
+  --worker-class gthread \
+  --log-level info \
+  --access-logfile - &
 
 echo "Starting Nginx..."
 nginx -g "daemon off;"
